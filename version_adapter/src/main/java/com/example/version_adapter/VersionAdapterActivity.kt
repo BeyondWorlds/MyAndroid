05131f94b2ef4afa11560e735cafdb57c19bb234
wuqiang <wuqiang1@zzpxx.com>
2021-03-15 16:00:47 +0800

    版本适配Activitiy创建

---
+++ version_adapter/src/main/java/com/example/version_adapter/VersionAdapterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.version_adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VersionAdapterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_version_adapter)
+    }
+}
