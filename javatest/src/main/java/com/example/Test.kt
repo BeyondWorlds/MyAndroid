1410cadfc322c900fd63f0b9dabea525944c8e82
wuqiang <wuqiang1@zzpxx.com>
2021-08-16 11:59:56 +0800

    test

--- javatest/src/main/java/com/example/Test.kt
+++ javatest/src/main/java/com/example/Test.kt
@@ -12,20 +12,23 @@ import javax.xml.bind.JAXBElement
 object Test {
     @JvmStatic
     fun main(args: Array<String>) = runBlocking<Unit> {
-        CoroutineScope(Dispatchers.IO).launch {
-            var msg = ""
-            System.out.println("Hello bey")
+        launch {
+            if(isActive)
+            delay(10000L)
+            println("Task from runBlocking")
         }
-        makeTest {
-            System.out.println("makeTest")
-            return@makeTest
-        }
-        delay(5000)
-        System.out.println("end")
-    }
 
-    inline fun makeTest(success: () -> Unit) {
-        success()
-        System.out.println("success")
+        coroutineScope { // 创建一个协程作用域
+            launch {
+                delay(5000L)
+                println("Task from nested launch")
+            }
+
+            delay(100L)
+            println("Task from coroutine scope") // 这一行会在内嵌 launch 之前输出
+        }
+        //会等待coroutineScope执行完成，才走下面的语句，coroutineScope会等待子协程都执行完成，才会执行下面的语句
+        println("Coroutine scope is no over")
+        println("Coroutine scope is over") // 这一行在内嵌 launch 执行完毕后才输出
     }
 }
